dbf175b3a4f70f0e8844f9da9dab378207fcb9d6
CollageID <patelnishtha78@gmail.com>
2025-09-09 01:28:46 +0530

    modify_data

---
+++ controller/temp.js
@@ -0,0 +1,14 @@
+// const fs=require('fs');
+// const path=require('path');
+
+// fs.writeFile(
+//             path.join(__dirname,"../MOCK_DATA.json"),
+//             JSON.stringify(data),
+//             (err)=>{
+//                 if(err){
+//                     console.log("Error Writing file:",err);
+//                     return res.status(500).json("Something Went Wrong!!");
+//                 }
+//                 res.json("SuccessFully user added");
+//             }
+//         )
